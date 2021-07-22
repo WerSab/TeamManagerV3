e4cdd71c3521e79f0d42e14b2a07222d741e1519
Weronika Sabor <weronika.sabor@interia.pl>
2021-07-22 14:01:26 +0200

    CustomFlatListTurnieje_update

--- TeamManagerV3/src/components/CustomFlatList_turnieje.js
+++ TeamManagerV3/src/components/CustomFlatList_turnieje.js
@@ -12,21 +12,22 @@ import {
 } from 'react-native';
 import {connect} from 'react-redux';
 import {turniejeActions} from '../store';
+import {userActions} from '../store';
 import deleteIcon from '../../assets/icons/delete.png/';
-import CustomFlatList from '../components/CustomFlatList';
+import CustomFlatList_team from './CustomFlatList_team';
 
 const CustomFlatList_turnieje = ({
   //propsy do flatlisty
+  data,
   category,
   deleteElement,
   withSearchbar,
   turnieje,
   user,
-  data,
 }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [flatListData, setFlatListData] = useState(data);
-  const [roundCardData, setRoundCardData] = useState(data);
+  const [roundListData, setRoundListData] = useState(data);
   const [searchInputValue, setSearchInputValue] = useState('');
 
   const deleteAlert = (id, name) => {
@@ -64,36 +65,45 @@ const CustomFlatList_turnieje = ({
     setFlatListData(newData);
   };
 
-  const RoundCard = selectedId => {
-    const round = data?.filter(item => {
-      const itemData = item.id;
-      const idData = selectedId;
-      return itemData === idData;
-    });
-    setRoundCardData(round);
-  };
+  const RoundCard = (selectedId) => {
+    const rounds = data?.filter(item => {
+      const itemData = item.id
+      const idData = selectedId
+      return itemData === idData
+    })
+    setRoundListData(rounds)
+  }
 
   const renderItem = item => {
-    const backgroundColor = 'e8e5e8';
-    const textColor = 'black';
     return (
-      <View style={styles.container} key={item.id.toString()}>
-        <TouchableOpacity style={styles.row}>
+      <>
+        <View style={styles.button} key={item.id.toString()}>
           <TouchableOpacity
-            style={[styles.item, backgroundColor]}
+            style={styles.name}
             onPress={() => {
               setIsModalVisible(true);
-              RoundCard();
+              RoundCard(item.id);
             }}>
-            <Text style={[styles.title, textColor]}>{item.name}</Text>
+            <Text numberOfLines={1} style={[styles.textStyleBig]}>
+              {item.id} {item.name}
+            </Text>
           </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => deleteAlert(item.id, item.name)}
-            style={[styles.item, backgroundColor]}>
+
+          <TouchableOpacity style={styles.buttonBreakHeight}></TouchableOpacity>
+
+          <TouchableOpacity onPress={() => deleteAlert(item.id, item.name)}>
             <Image source={deleteIcon} style={styles.icon} />
           </TouchableOpacity>
-        </TouchableOpacity>
-      </View>
+
+
+
+          <FlatList
+                  data={item.playersId}
+                  renderItem={({ item }) => <Text>{item.id}</Text>}
+                  keyExtractor={item => item.id}
+                />
+        </View>
+      </>
     );
   };
 
@@ -110,13 +120,14 @@ const CustomFlatList_turnieje = ({
             <TouchableOpacity>
               <Text style={styles.textStyleBig}>Round Card</Text>
               <FlatList
-                data={roundCardData}
+                data={roundListData}
                 category={category}
                 // ListHeaderComponent={renderSearchBar || null}
                 renderItem={({item}) => renderItem(item)}
                 keyExtractor={(item, index) => index.toString()}
                 style={{flex: 1}}
               />
+
               <View style={styles.buttonRow}>
                 <TouchableOpacity
                   style={styles.buttonClose}
@@ -141,8 +152,10 @@ const CustomFlatList_turnieje = ({
       {withSearchbar ? renderSearchBar() : null}
       <FlatList
         data={flatListData}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
+        category={category}
+        renderItem={({item}) => renderItem(item)} //do renderItem przekazujemy wartośc funkcji renderItem
+        keyExtractor={(item, index) => index.toString()}
+        style={{flex: 2}}
         withSearchbar={true}
       />
     </View>
@@ -160,25 +173,121 @@ const mapDispatch = dispatch => ({
 const styles = StyleSheet.create({
   container: {
     flex: 2,
+    justifyContent: 'center',
+    width: '100%',
+    backgroundColor: 'white',
+  },
+  itemContainer: {
     flexDirection: 'column',
+    flex: 2,
+    justifyContent: 'center',
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    width: '100%',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    width: '90%',
+    marginLeft: 4,
+  },
+  nameContainer: {
+    flexBasis: '50%',
+    backgroundColor: '#2a343f',
+    borderRadius: 5,
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    flexBasis: '10%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonJoin: {
+    borderRadius: 5,
+    paddingVertical: 1,
+    paddingHorizontal: 1,
+    justifyContent: 'flex-end',
+    elevation: 2,
+    width: 30,
+    backgroundColor: '#f8fc05',
+    justifyContent: 'flex-end',
+  },
+  buttonConfirm: {
+    borderRadius: 5,
+    paddingVertical: 1,
+    paddingHorizontal: 1,
+    justifyContent: 'flex-end',
+    elevation: 2,
+    width: 55,
+    backgroundColor: '#32fc05',
+  },
+
+  button: {
+    flexDirection: 'row',
     borderRadius: 10,
     paddingVertical: -5,
     paddingHorizontal: -5,
+    elevation: 1,
     width: '100%',
     backgroundColor: '#eeedef',
     justifyContent: 'space-between',
   },
-  row: {
-    flex: 2,
-    flexDirection: 'row',
+
+  textStyleBig: {
+    color: 'black',
+    fontSize: 18,
+
+    textAlign: 'center',
   },
-  item: {
-    padding: 5,
-    marginVertical: 8,
-    marginHorizontal: 3,
+
+  date: {
+    color: 'black',
+    fontSize: 12,
+    fontWeight: '200',
+    textAlign: 'center',
   },
-  title: {
-    fontSize: 15,
+  name: {
+    fontSize: 12,
+    color: 'black',
+    paddingHorizontal: 5,
+    paddingVertical: 20,
+  },
+
+  icon: {
+    height: 20,
+    width: 20,
+    justifyContent: 'flex-end',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+  },
+  buttonClose: {
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    //elevation: 2,
+    width: 120,
+    backgroundColor: '#CCCCCC',
+  },
+  buttonSafe: {
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    //elevation: 2,
+    width: 120,
+    backgroundColor: '#FCA542',
   },
 });
 export default connect(mapState, mapDispatch)(CustomFlatList_turnieje);
